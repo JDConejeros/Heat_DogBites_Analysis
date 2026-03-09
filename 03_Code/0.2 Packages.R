f2b5047cac888fd0e3edb615a7f1ce557a02b3ec
José Daniel Conejeros <43749364+JDConejeros@users.noreply.github.com>
2026-03-09 10:17:52 -0300

    Add init codes

---
+++ 03_Code/0.2 Packages.R
@@ -0,0 +1,39 @@
+# Packages ---- 
+
+# Function install/load packages
+install_load <- function(packages){
+  for (i in packages) {
+    if (i %in% rownames(installed.packages())) {
+      library(i, character.only=TRUE)
+    } else {
+      install.packages(i)
+      library(i, character.only = TRUE)
+    }
+  }
+}
+
+# Apply function
+install_load(c("rio", 
+               "data.table",
+               "tidyverse",
+               "chilemapas",
+               "future",
+               "furrr",
+               "ragg",
+               "writexl",
+               "survival",
+               "tictoc",
+               "knitr",
+               "classInt",
+               "RColorBrewer",
+               "spatialEco",
+               "splines",
+               "tictoc",
+               "future",
+               "future.apply",
+               "ggpubr",
+               "ggplot2", 
+               "sf", 
+               "ggmap", 
+               "maptiles"
+               ))
